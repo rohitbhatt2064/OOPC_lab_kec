e22f156e28234527c78ae3d78992760dd6875177
rohitbhatt2064 <rohitbhatt2064@users.noreply.github.com>
2026-06-13 21:56:43 +0545

    working in file

--- SelfLearningAndWork/Project1/Project1/Log.cpp
+++ SelfLearningAndWork/Project1/Project1/Log.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+
+#include <iostream>
 
 void logr(const char* text)
 {
--- SelfLearningAndWork/Project1/Project1/Math.cpp
+++ SelfLearningAndWork/Project1/Project1/Math.cpp
@@ -1,16 +1,19 @@
-
 #include <iostream>
 
-void logr(const char* text);
+//void logr(const char* text);
 
-int Multiply(int a, int b)
+void log(const char* text);
+
+static int Multiply(int a, int b)
 
 {
 	int result = a * b;
+	log("Multlipy");
 	return result;
 }
 
 int main()
 {
-
+	//std::cout << Multiply(5, 8) << std::endl;
+	std::cin.get();
 }
