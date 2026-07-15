f09d70e05fa11883936d6642af5ce98081bba574
rohitbhatt2064 <rohitbhatt2064@users.noreply.github.com>
2026-07-15 16:02:56 +0545

    New Program Added

---
+++ Template/TemplateFunctopnWithNonTemplateTypeArguement.cpp
@@ -0,0 +1,14 @@
+/* defining template type function with non templeate type arguement */
+
+#include<iostream>
+
+using namespace std;
+
+template < class T , int x > // class T is generic /template datatype and int is integer datatype
+// if we dont know type keep generic if we know then use datatype keep that we have to do same for return type
+
+
+int main()
+{
+
+}
