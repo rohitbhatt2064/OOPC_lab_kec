7a9105372f1eac4197994f7a9499a548bf88a71a
rohitbhatt2064 <rohitbhatt2064@users.noreply.github.com>
2026-07-15 16:01:03 +0545

    New Program Added

---
+++ Template/ReturnPowerOfAnyData.cpp
@@ -0,0 +1,14 @@
+/* create a function that will return power of any user given data */
+
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+template < class T , int X >
+
+T Power ( T data )
+{
+    return pow(data , power )
+}
